d9d8414cb2761bdc8f2f69a2aeaf87ae321b4001
taozhaolib <tao.zhao.lib@gmail.com>
2016-06-13 15:19:17 -0500

    added some json to java collection object conversion functions

--- config/src/main/java/org/shareok/data/config/DataUtil.java
+++ config/src/main/java/org/shareok/data/config/DataUtil.java
@@ -5,11 +5,22 @@
  */
 package org.shareok.data.config;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author Tao Zhao
  */
 public class DataUtil {
+    
+    private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(DataUtil.class);
+    
     public static String[] REPO_TYPES = {"unknown", "dspace", "islandora", "fedora", "hydra"};
     public static String[] JOB_TYPES = {"unknown", "ssh-import", "rest-import", "ssh-upload", "ssh-import-uloaded"};
     
@@ -56,5 +67,34 @@ public class DataUtil {
         else {
             return 0;
         }
+    }
+    
+    public static Map getMapFromJson(String json){
+        Map<String, Object> map = new HashMap<>();
+        try{
+            ObjectMapper mapper = new ObjectMapper();        
+            map = mapper.readValue(json, new TypeReference<Map<String, Object>>(){});
+        } catch (IOException ex) {
+            logger.error("Cannot convert the json string into map", ex);
+        }
+        return map;
+    }
+    
+    /**
+     * Converts an array like json string into a list
+     * 
+     * @param json : like a Sting [{info1:val1}, {info2:val2}]
+     * @return : list of hashmaps
+     */
+    public static List<Map<String, Object>> getListFromJson(String json){
+        List<Map<String, Object>> list = new ArrayList<>();
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            list = mapper.readValue(json, new TypeReference<ArrayList<HashMap<String, Object>>>(){});
+        }
+        catch(Exception ex){
+            logger.error("Cannot convert the json string into list of hashmaps", ex);
+        }
+        return list;
     }
 }
